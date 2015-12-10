c130b9606453ae9ecee92a6c6f6fee120065e626
Andrei Stepanov <astepano@redhat.com>
2015-12-10 17:28:56 +0100

    Rewrite rv_input.py
    Signed-off-by: Andrei Stepanov <astepano@redhat.com>

--- spice/tests/rv_input.py
+++ spice/tests/rv_input.py
@@ -1,35 +1,45 @@
 """
-rv_input.py - test keyboard inputs through spice
+Test keyboard inputs through spice.
 
-Requires: Two VMs - client and guest and remote-viewer session
-          from client VM to guest VM created by rv_connect test.
-          Deployed PyGTK on guest VM.
+Requires
+--------
+    - Two VMs - client and guest.
+    - remote-viewer session from client VM to guest VM created by rv_connect
+      test.
+    - Deployed PyGTK on guest VM.
 
-          Presumes the numlock state at startup is 'OFF'.
+Presumes the numlock state at startup is 'OFF'.
 """
 
 import logging
 import os
-from aexpect import ShellCmdError
-from autotest.client.shared import error
-from virttest import utils_misc, utils_spice
-from spice.tests.rv_session import *
+import aexpect
+from avocado.core import exceptions
+from virttest import utils_misc
+from spice.lib import utils_spice
+from spice.lib import rv_session
+from spice.lib import conf
+
 
 def install_pygtk(guest_session, params):
     """
     Install PyGTK to a VM with yum package manager.
 
-    @param guest_session - ssh session to guest VM
-    @param params
+    Parameters
+    ----------
+    guest_session :
+        Ssh session to guest VM.
+    params : virttest.utils_params.Params
+        Dictionary with the test parameters.
     """
 
     cmd = "rpm -q pygtk2"
     try:
         guest_session.cmd(cmd)
-    except ShellCmdError:
+    except aexpect.ShellCmdError:
         cmd = "yum -y install pygtk2 --nogpgcheck > /dev/null"
         logging.info("Installing pygtk2 package to %s",
-                    params.get("guest_vm"))
+                     params.get("guest_vm"))
         guest_session.cmd(cmd, timeout=60)
 
 
@@ -38,9 +48,14 @@ def deploy_test_form(test, guest_vm, params):
     Copy wxPython Test form to guest VM.
     Test form is copied to /tmp directory.
 
-    @param test
-    @param guest_vm - vm object
-    @param params
+    Parameters
+    ----------
+    test :
+        xxx
+    guest_vm :
+        Vm object.
+    params : virttest.utils_params.Params
+        Dictionary with the test parameters.
     """
 
     script = params.get("guest_script")
@@ -55,10 +70,13 @@ def run_test_form(guest_session, params):
     Start wxPython simple test form on guest VM.
     Test form catches KeyEvents and is located in /tmp.
 
-    @param guest_session - ssh session to guest VM
-    @param params
+    Parameters
+    ----------
+    guest_session :
+        Ssh session to guest VM.
+    params : virttest.utils_params.Params
+        Dictionary with the test parameters.
     """
-
     logging.info("Starting test form for catching key events on guest")
     cmd = "python /tmp/%s &> /dev/null &" % params.get("guest_script")
     guest_session.cmd(cmd)
@@ -68,12 +86,13 @@ def run_test_form(guest_session, params):
 
 def get_test_results(guest_vm):
     """
-    @param guest_vm - vm object
+    Parameters
+    ----------
+    guest_vm :
+        Vm object.
     """
-
     path = "/tmp/autotest-rv_input"
     guest_vm.copy_files_from(path, path, timeout=60)
-
     return path
 
 
@@ -82,14 +101,17 @@ def test_type_and_func_keys(client_vm, guest_session, params):
     Test typewriter and functional keys.
     Function sends various keys through qemu monitor to client VM.
 
-    @param client_vm - vm object
-    @param guest_session - ssh session to guest VM
-    @param params
+    Parameters
+    ----------
+    client_vm :
+        Vm object.
+    guest_session :
+        Ssh session to guest VM.
+    params : virttest.utils_params.Params
+        Dictionary with the test parameters.
     """
-
     run_test_form(guest_session, params)
     utils_spice.wait_timeout(3)
-
     # Send typewriter and functional keys to client machine based on scancodes
     logging.info("Sending typewriter and functional keys to client machine")
     for i in range(1, 69):
@@ -104,15 +126,18 @@ def test_leds_and_esc_keys(client_vm, guest_session, params):
     Test LEDS and Escaped keys.
     Function sends various keys through qemu monitor to client VM.
 
-    @param client_vm - vm object
-    @param guest_session - ssh session to guest VM
-    @param params
+    Parameters
+    ----------
+    client_vm :
+        Vm object.
+    guest_session :
+        Ssh session to guest VM.
+    params : virttest.utils_params.Params
+        Dictionary with the test parameters.
     """
-
     #Run PyGTK form catching KeyEvents on guest
     run_test_form(guest_session, params)
     utils_spice.wait_timeout(3)
-
     # Prepare lists with the keys to be sent to client machine
     leds = ['a', 'caps_lock', 'a', 'caps_lock', 'num_lock', 'kp_1', 'num_lock',
             'kp_1']
@@ -120,9 +145,7 @@ def test_leds_and_esc_keys(client_vm, guest_session, params):
                  'alt-x']
     escaped = ['insert', 'delete', 'home', 'end', 'pgup', 'pgdn', 'up',
                'down', 'right', 'left']
-
     test_keys = leds + shortcuts + escaped
-
     # Send keys to client machine
     logging.info("Sending leds and escaped keys to client machine")
     for key in test_keys:
@@ -135,15 +158,18 @@ def test_nonus_layout(client_vm, guest_session, params):
     Test some keys of non-us keyboard layouts (de, cz).
     Function sends various keys through qemu monitor to client VM.
 
-    @param client_vm - vm object
-    @param guest_session - ssh session to guest VM
-    @param params
+    Parameters
+    ----------
+    client_vm :
+        Vm object.
+    guest_session :
+        Ssh session to guest VM.
+    params : virttest.utils_params.Params
+        Dictionary with the test parameters.
     """
-
     #Run PyGTK form catching KeyEvents on guest
     run_test_form(guest_session, params)
     utils_spice.wait_timeout(3)
-
     # Czech layout - test some special keys
     cmd = "setxkbmap cz"
     guest_session.cmd(cmd)
@@ -152,7 +178,6 @@ def test_nonus_layout(client_vm, guest_session, params):
     for key in test_keys:
         client_vm.send_key(key)
         utils_spice.wait_timeout(0.3)
-
     # German layout - test some special keys
     cmd = "setxkbmap de"
     guest_session.cmd(cmd)
@@ -161,7 +186,6 @@ def test_nonus_layout(client_vm, guest_session, params):
     for key in test_keys:
         client_vm.send_key(key)
         utils_spice.wait_timeout(0.3)
-
     cmd = "setxkbmap us"
     guest_session.cmd(cmd)
 
@@ -172,35 +196,34 @@ def test_leds_migration(client_vm, guest_vm, guest_session, params):
     Function sets LEDS (caps, num) to ON and send scancodes of "a" and "1 (num)"
     and expected to get keycodes of "A" and "1" after migration.
 
-    @param client_vm - vm object
-    @param guest_vm - vm object
-    @param guest_session - ssh session to guest VM
-    @param params
+    Parameters
+    ----------
+    client_vm :
+        Vm object.
+    guest_vm :
+        Vm object.
+    guest_session :
+        Ssh session to guest VM.
+    params : virttest.utils_params.Params
+        Dictionary with the test parameters.
     """
-
     # Turn numlock on RHEL6 on before the test begins:
     grep_ver_cmd = "grep -o 'release [[:digit:]]' /etc/redhat-release"
     rhel_ver = guest_session.cmd(grep_ver_cmd).strip()
-
     logging.info("RHEL version: #{0}#".format(rhel_ver))
-
     if rhel_ver == "release 6":
         client_vm.send_key('num_lock')
-
     #Run PyGTK form catching KeyEvents on guest
     run_test_form(guest_session, params)
     utils_spice.wait_timeout(3)
-
     # Tested keys before migration
     test_keys = ['a', 'kp_1', 'caps_lock', 'num_lock', 'a', 'kp_1']
     logging.info("Sending leds keys to client machine before migration")
     for key in test_keys:
         client_vm.send_key(key)
         utils_spice.wait_timeout(0.3)
-
     guest_vm.migrate()
     utils_spice.wait_timeout(8)
-
     #Tested keys after migration
     test_keys = ['a', 'kp_1', 'caps_lock', 'num_lock']
     logging.info("Sending leds keys to client machine after migration")
@@ -214,10 +237,13 @@ def analyze_results(file_path, test_type):
     """
     Analyze results - compare caught keycodes and expected keycodes.
 
-    @param file_path - path to file with results
-    @param test_type - type of the test
+    Parameters
+    ----------
+    file_path :
+        Path to file with results.
+    test_type :
+        Type of the test.
     """
-
     if test_type == "type_and_func_keys":
         #List of expected keycodes from guest machine
         correct_keycodes = ['65307', '49', '50', '51', '52', '53', '54', '55',
@@ -228,7 +254,7 @@ def analyze_results(file_path, test_type):
                             '108', '59', '39', '96', '92', '122', '120', '99',
                             '118', '98', '110', '109', '44', '46', '47', '32',
                             '65470', '65471', '65472', '65473', '65474',
-                             '65475', '65476', '65477', '65478', '65479']
+                            '65475', '65476', '65477', '65478', '65479']
     elif test_type == "leds_and_esc_keys":
         correct_keycodes = ['97', '65509', '65', '65509', '65407', '65457',
                             '65407', '65436', '97', '65505', '65', '65506',
@@ -243,61 +269,60 @@ def analyze_results(file_path, test_type):
     elif test_type == "leds_migration":
         correct_keycodes = ['97', '65457', '65509', '65407', '65', '65436',
                             '65', '65436', '65509', '65407']
-
     # Read caught keycodes on guest machine
     fileobj = open(file_path, "r")
     keycodes = fileobj.read()
     fileobj.close()
-
     #Compare caught keycodes with expected keycodes
     test_keycodes = keycodes.split()
     logging.info("Caught keycodes:%s", test_keycodes)
     for i in range(len(correct_keycodes)):
-        if not (test_keycodes[i] == correct_keycodes[i]):
+        if not test_keycodes[i] == correct_keycodes[i]:
             return correct_keycodes[i]
-
     return None
 
 
-def run_rv_input(test, params, env):
+def run(test, params, env):
     """
     Test for testing keyboard inputs through spice.
 
-    @param test: QEMU test object.
-    @param params: Dictionary with the test parameters.
-    @param env: Dictionary with test environment.
+    Parameters
+    ----------
+    test : avocado.core.plugins.vt.VirtTest
+        QEMU test object.
+    params : virttest.utils_params.Params
+        Dictionary with the test parameters.
+    env : virttest.utils_env.Env
+        Dictionary with test environment.
+
+    Raises
+    ------
+    TestFail
+        Test fails for expected behaviour.
     """
-
-    session = RvSession(params, env)
+    session = rv_session.RvSession(params, env)
     session.clear_interface_all()
-
     guest_vm = session.guest_vm
-    guest_session = guest_vm.wait_for_login(
-        timeout=int(params.get("login_timeout", 360)),
-        username="root", password="123456")
-
+    timeout = int(params.get("login_timeout", 360))
+    guest_session = guest_vm.wait_for_login(timeout=timeout,
+                                            username=conf.USERNAME,
+                                            password=conf.PASSWORD)
     client_vm = session.client_vm
-
-    client_session = client_vm.wait_for_login(
-        timeout=int(params.get("login_timeout", 360)),
-        username="root", password="123456")
-
-    guest_root_session = guest_vm.wait_for_login(
-              timeout=int(params.get("login_timeout", 360)),
-              username="root", password="123456")
-
-    # Verify that gnome is now running on the guest
+    client_session = client_vm.wait_for_login(timeout=timeout,
+                                              username=conf.USERNAME,
+                                              password=conf.PASSWORD)
+    guest_root_session = guest_vm.wait_for_login(timeout=timeout,
+                                                 username=conf.USERNAME,
+                                                 password=conf.PASSWORD)
     try:
         guest_session.cmd("ps aux | grep -v grep | grep gnome-session")
-    except ShellCmdError:
-        raise error.TestWarn("gnome-session was probably not corretly started")
-
+    except aexpect.ShellCmdError:
+        raise exceptions.TestWarn(
+            "gnome-session was probably not corretly started"
+        )
     guest_session.cmd("export DISPLAY=:0.0")
-
     install_pygtk(guest_root_session, params)
-
     deploy_test_form(test, guest_vm, params)
-
     # Get test type and perform proper test
     test_type = params.get("config_test")
     test_mapping = {'type_and_func_keys': test_type_and_func_keys,
@@ -309,29 +334,23 @@ def run_rv_input(test, params, env):
         'leds_and_esc_keys': (client_vm, guest_session, params),
         'nonus_layout': (client_vm, guest_session, params),
         'leds_migration': (client_vm, guest_vm, guest_session, params)}
-
     try:
         func = test_mapping[test_type]
         args = test_parameters[test_type]
     except:
-        raise error.TestFail("Unknown type of test")
-
+        raise exceptions.TestFail("Unknown type of test")
     session.connect()
-
     try:
         session.is_connected()
     except:
         logging.info("FAIL")
-        raise error.TestFail("Failed to establish connection")
-
+        raise exceptions.TestFail("Failed to establish connection")
     func(*args)
-
     # Get file with caught keycodes from guest
     result_path = get_test_results(guest_vm)
     # Analyze results and raise fail exp. If sent scancodes
     # do not match with expected keycodes
     result = analyze_results(result_path, test_type)
     if result is not None:
-        raise error.TestFail("Testing of sending keys failed:"
-                             "  Expected keycode = %s" % result)
-
+        raise exceptions.TestFail("Testing of sending keys failed:"
+                                  "  Expected keycode = %s" % result)
